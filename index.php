4f8cc5b1bbe64895504bb461de9af6c0c1834b87
CadanDev <cadan.dev@gmail.com>
2026-03-30 19:48:58 -0300

    Ajustar a formatação do código e incluir o carregamento do arquivo de ambiente
    Signed-off-by: CadanDev <cadan.dev@gmail.com>

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
+
 use Api\Controllers\ApiController;
 
 $config = require_once __DIR__ . '/config/api.php';
+$config = require_once __DIR__ . '/config/env.php';
 
 /**
  * Initialize error reporting for development.
  * In production, these settings should be adjusted to log errors instead of displaying them.
  */
-if($config['environment'] === 'dev') {
-	ini_set('display_errors', '1');
-	ini_set('display_startup_errors', '1');
-	error_reporting(E_ALL);
+if ($config['environment'] === 'dev') {
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
 } else {
-	ini_set('display_errors', '0');
-	error_reporting(0);
+    ini_set('display_errors', '0');
+    error_reporting(0);
 }
 
 /**
@@ -21,17 +23,17 @@ if($config['environment'] === 'dev') {
  * This ensures that even if a fatal error occurs, the client receives a structured error message.
  */
 register_shutdown_function(function () {
-	$err = error_get_last();
-	if ($err !== null) {
-		http_response_code(500);
-		header('Content-Type: application/json');
-		echo json_encode([
-			'error' => 'Fatal error',
-			'message' => $err['message'] ?? null,
-			'file' => $err['file'] ?? null,
-			'line' => $err['line'] ?? null,
-		]);
-	}
+    $err = error_get_last();
+    if ($err !== null) {
+        http_response_code(500);
+        header('Content-Type: application/json');
+        echo json_encode([
+            'error' => 'Fatal error',
+            'message' => $err['message'] ?? null,
+            'file' => $err['file'] ?? null,
+            'line' => $err['line'] ?? null,
+        ]);
+    }
 });
 
 require_once __DIR__ . '/autoload.php';
